d3c2872d78254406af646775cb5359def2e69fc2
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-07-20 17:30:30 +0300

    выполнил дз

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
+import cards from './templates/cards.hbs'
+import menu from './menu.json'
 import './sass/main.scss';
+
+
+
+const menuRef = document.querySelector('.js-menu')
+const themeSwichRef = document.querySelector('#theme-switch-toggle')
+
+const cardMarkup = createMenuCards(menu)
+menuRef.insertAdjacentHTML('beforeend', cardMarkup)
+
+themeSwichRef.addEventListener('click', onThemeSwich)
+
+themeCheck()
+
+function createMenuCards(menu) {
+    return menu.map(cards).join('')
+}
+
+function themeCheck() {
+    if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-theme')
+    themeSwichRef.checked = true;
+}
+}
+
+function onThemeSwich(event) {
+        const bodyClasslist = document.body.classList
+        localStorage.setItem('dark-mode', event.target.checked)   
+    if (localStorage.getItem('dark-mode') === 'true') {
+            bodyClasslist.remove('light-theme')
+            bodyClasslist.add('dark-theme')
+    } else {
+            bodyClasslist.remove('dark-theme')
+            bodyClasslist.add('light-theme')
+        }
+}
+
+

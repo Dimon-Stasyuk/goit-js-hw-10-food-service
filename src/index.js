1b77080265ba952e3a3c99ee95599a370489bf85
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-07-21 17:05:39 +0300

    сделал версию попроще

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
-import cards from './templates/cards.hbs'
-import menu from './menu.json'
 import './sass/main.scss';
+import cardTemplate from './templates/cards.hbs'
+import cards from './menu.json'
+
+const menuListRef = document.querySelector('.js-menu')
+const themeSwitchRef = document.querySelector('.theme-switch__toggle')
+
+darkThemeCheck()
+
+menuListRef.insertAdjacentHTML('beforeend', cardTemplate(cards))
+
+themeSwitchRef.addEventListener('click',onThemeSwitch)
 
 
-
-const menuRef = document.querySelector('.js-menu')
-const themeSwichRef = document.querySelector('#theme-switch-toggle')
-
-const cardMarkup = createMenuCards(menu)
-menuRef.insertAdjacentHTML('beforeend', cardMarkup)
-
-themeSwichRef.addEventListener('click', onThemeSwich)
-
-themeCheck()
-
-function createMenuCards(menu) {
-    return menu.map(cards).join('')
-}
-
-function themeCheck() {
+function darkThemeCheck() {
     if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-theme')
-    themeSwichRef.checked = true;
+    themeSwitchRef.checked = true
 }
 }
 
-function onThemeSwich(event) {
-        const bodyClasslist = document.body.classList
-        localStorage.setItem('dark-mode', event.target.checked)   
-    if (localStorage.getItem('dark-mode') === 'true') {
-            bodyClasslist.remove('light-theme')
-            bodyClasslist.add('dark-theme')
-    } else {
-            bodyClasslist.remove('dark-theme')
-            bodyClasslist.add('light-theme')
-        }
+function onThemeSwitch(event) {
+    document.body.classList.toggle('dark-theme')
+    localStorage.setItem('dark-mode', event.target.checked)  
 }
-
 
